6b13cc8c32b2d07a85790d18395e91bec5785267
Éder Porto <eder.porto@hotmail.com>
2023-11-14 19:21:37 -0300

    Adjusting the Skills app

--- skills/models.py
+++ skills/models.py
@@ -1,25 +1,31 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator
+from django.utils import timezone
 
-class Skill_Family(models.Model):
-    name = models.CharField(max_length=128)
-    profile_image = models.URLField(max_length=256)
-    profile_header = models.URLField(max_length=256)
-    description = models.CharField(max_length=512)
-    wiki_link = models.URLField(max_length=256)
-    date_of_creation = models.DateField()
-    date_of_last_edit = models.DateField()
-    def __str__(self):
-        return self.name
+
+qid_form_validator = RegexValidator(regex=r"^Q\d+$", message=_("Field must be in the format \"Q123456789\""))
+
 
 class Skill(models.Model):
-    name = models.CharField(max_length=128)
-    profile_image = models.URLField(max_length=256)
-    profile_header = models.URLField(max_length=256)
-    description = models.CharField(max_length=512)
-    wiki_link = models.URLField(max_length=256)
-    date_of_creation = models.DateField()
-    date_of_last_edit = models.DateField()
-    self_learning = models.URLField(max_length=256)
-    family = models.ForeignKey(Skill_Family, null=False, on_delete=models.CASCADE)
+    skill_name = models.CharField(_("Name"), max_length=128)
+    skill_description = models.CharField(_("Description"), max_length=1000)
+    skill_type = models.ManyToManyField("self", verbose_name=_("Skill type"))
+    skill_wikidata_item = models.CharField(_("Wikidata item associated"), max_length=30, null=True, blank=True,
+                                           validators=[qid_form_validator])
+    skill_date_of_creation = models.DateTimeField(default=timezone.now)
+
     def __str__(self):
-        return self.name
+        return self.skill_name
+
+
+class Service(models.Model):
+    service_name = models.CharField(_("Name"), max_length=128)
+    service_description = models.TextField(_("Description"), max_length=1000)
+    service_type = models.ManyToManyField("self", verbose_name=_("Service type"))
+    service_wikidata_item = models.CharField(_("Wikidata item associated"), max_length=30, null=True, blank=True,
+                                             validators=[qid_form_validator])
+    service_date_of_creation = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.service_name
