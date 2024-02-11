47dc665c99038daa3d94751bbf6b807e62804649
albertoleoncio <albertoleoncio@hotmail.com>
2024-02-11 14:19:29 -0300

    +Styles

--- users/forms.py
+++ users/forms.py
@@ -4,4 +4,7 @@ from .models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
+        widgets = {
+            'birthday': forms.DateTimeInput(attrs={'type': 'date'})
+        }
         exclude = ['user']  # Exclude user field since it's linked to CustomUser
