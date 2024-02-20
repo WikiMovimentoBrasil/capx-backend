43ecde188ebd6a99bc57d7949f406d69c39abb41
albertoleoncio <albertoleoncio@hotmail.com>
2024-02-20 20:43:09 -0300

    --

--- CapX/settings_local.py
+++ CapX/settings_local.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from django.utils.translation import gettext_lazy as _
 
-DEBUG = True #Change to False when in production
+DEBUG = False #Change to False when in production
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
