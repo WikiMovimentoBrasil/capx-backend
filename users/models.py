44f83906791084a12c68fa4c62c1bb4ef8073b7f
albertoleoncio <albertoleoncio@hotmail.com>
2024-02-21 17:39:56 -0300

    test

--- users/models.py
+++ users/models.py
@@ -161,4 +161,7 @@ class Profile(models.Model):
 @receiver(post_save, sender=CustomUser)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        try:
+            Profile.objects.create(user=instance)
+        except AuthorizationException as e:
+            print("OpenSearch update failed:", e)
