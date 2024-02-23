d579ea890a3d4c9cb0be1a36cccc6284cdd65dce
Ikeadeoyin <ikeadeoyin@gmail.com>
2024-02-23 20:45:47 +0100

    views documentation

--- bugs/views.py
+++ bugs/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render, redirect, reverse,  get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, permission_required
-from .forms import  BugForm
-
-from django.views.generic import ListView, CreateView  # new
-from django.urls import reverse_lazy  # new
-
-# from .forms import BugForm  # new
+from .forms import BugForm
 from .models import Bug, Attachment
-# Create your views here.
 
 
-# bugs homepage
 @login_required
 def homepage(request):
-    # context = {}
-    # return render(request, "bugs/index.html", context)
+    """
+      View to display the bugs reporting homepage
+      :param request: 
+      :return: returns all bugs and a link to submit a new bug report
+    """
     bugs = Bug.objects.filter(user=request.user)  # Get all bugs from the database
     return render(request, 'bugs/bug_list.html', {'bugs': bugs})
 
 
 @login_required()
 def bug_form(request):
+    """
+    View to submit a new bug report
+    :param request:
+    :return: returns all bugs including the new bug report
+    """
     if request.method == 'POST':
         form = BugForm(request.POST, request.FILES)
         if form.is_valid():
@@ -51,6 +52,11 @@ def bug_form(request):
 
 @login_required()
 def bug_list(request):
+    """
+     View to display all the bugs created by the authenticated user
+    :param request:
+    :return: list of all bugs created by the authenticated user
+    """
     if request.user.is_superuser:
         bugs = Bug.objects.all()  # Retrieves all bugs for superuser
     else:
@@ -60,6 +66,12 @@ def bug_list(request):
 
 @login_required()
 def bug_detail(request, bug_id):
+    """
+    View to display the details of a bug by id
+    :param request:bug i
+    :param bug_id:
+    :return: details about the bug
+    """
     bug = get_object_or_404(Bug, pk=bug_id)  # Fetch the bug or return a 404
     attachments = bug.attachments.all()  # Retrieve all related attachments
     return render(request, 'bugs/bug_detail.html', {'bug': bug, 'attachments': attachments})
@@ -67,6 +79,12 @@ def bug_detail(request, bug_id):
 
 @login_required()
 def update_bug(request, bug_id):
+    """
+    View to update a bug by id and its attachments
+    :param request:
+    :param bug_id: id of the bug to be updated
+    :return: details about the updated bug
+    """
     bug = get_object_or_404(Bug, pk=bug_id)
 
     if not (request.user == bug.user or request.user.is_superuser):
@@ -98,6 +116,12 @@ def update_bug(request, bug_id):
 @login_required()
 @permission_required('bugs.delete_bug')
 def delete_bug(request, bug_id):
+    """
+    Delete an existing bug by id
+    :param request:
+    :param bug_id:
+    :return: returns the list of all bugs after deleting the specified bug
+    """
     bug = get_object_or_404(Bug, pk=bug_id)
     bug.delete()
     messages.success(request, 'Bug deleted successfully!')
