995d1945669b9f8c982d0b8be88897287b337c49
Ikeadeoyin <ikeadeoyin@gmail.com>
2024-02-29 19:41:48 +0100

    added tests

--- bugs/tests/test_views.py
+++ bugs/tests/test_views.py
@@ -28,6 +28,7 @@ class HomePageViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "bugs/bug_list.html")
 
+
 class BugFormViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -61,8 +62,40 @@ class BugFormViewTest(TestCase):
         self.assertEqual(Bug.objects.count(), 1)
         self.assertRedirects(response, reverse('bugs:homepage'))
 
-
     def test_form_errors_for_invalid_data(self):
         response = self.client.post(reverse("bugs:register_bug"), {})
         self.assertFormError(response, 'form', 'title', 'This field is required.')
 
+
+class BugListTests(TestCase):
+    def setUp(cls):
+        # Create two users, one regular and one superuser
+        cls.user = User.objects.create_user(username='testuser', password='12345')
+        cls.superuser = User.objects.create_superuser(username='admin', email='admin@example.com', password='admin123')
+        # Create bugs for each user
+        Bug.objects.create(title="User Bug", description="A bug by a regular user", user=self.user)
+        Bug.objects.create(title="Admin Bug", description="A bug by an admin", user=self.superuser)
+
+    def test_redirect_if_not_logged_in(self):
+        # Ensure unauthenticated users are redirected to login
+        response = self.client.get(reverse('bugs:bug_list'))
+        self.assertRedirects(response, f'/accounts/login/?next={reverse("bugs:bug_list")}')
+
+    def test_logged_in_user_sees_own_bugs(self):
+        # Log in as a regular user
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(reverse('bugs:bug_list'))
+        # Check that the user sees only their bugs
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('bugs' in response.context)
+        self.assertEqual(len(response.context['bugs']), 1)
+        self.assertEqual(response.context['bugs'][0].user, self.user)
+
+    def test_superuser_sees_all_bugs(self):
+        # Log in as a superuser
+        self.client.login(username='admin', password='admin123')
+        response = self.client.get(reverse('bugs:bug_list'))
+        # Check that the superuser sees all bugs
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('bugs' in response.context)
+        self.assertEqual(len(response.context['bugs']), 2)  # Assuming only 2 bugs were created in setUp
