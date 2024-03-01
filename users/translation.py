a7f92da86534fb26da2f84b50ffd8174a3f31748
Éder Porto <eder.porto@hotmail.com>
2024-03-01 17:36:41 -0300

    Restarting migrations

--- users/translation.py
+++ users/translation.py
@@ -1,14 +1,9 @@
 from modeltranslation.translator import translator, TranslationOptions
-from .models import Organization, OrganizationType
-
-
-class OrganizationTranslationOptions(TranslationOptions):
-    fields = ('organization_name', 'organization_description')
+from .models import OrganizationType
 
 
 class OrganizationTypeTranslationOptions(TranslationOptions):
     fields = ('type_name', )
 
 
-translator.register(Organization, OrganizationTranslationOptions)
 translator.register(OrganizationType, OrganizationTypeTranslationOptions)
