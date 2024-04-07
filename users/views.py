c3c12c617c9bef7a0abbcdc500a1a4ad93d7c7d3
albertoleoncio <albertoleoncio@hotmail.com>
2024-04-07 13:44:54 -0300

    Users app: Disallow creations and deletions of users from API

--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
 from .models import Profile
 from .serializers import ProfileSerializer
-from rest_framework import status, viewsets, permissions, mixins
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
+    http_method_names = ['get', 'put', 'head', 'options']
 
+    def create(self, request):
+        response = {'message': 'Create function is not offered in this path.'}
+        return Response(response, status=status.HTTP_403_FORBIDDEN)
+
+    def destroy(self, request, pk=None):
+        response = {'message': 'Delete function is not offered in this path.'}
+        return Response(response, status=status.HTTP_403_FORBIDDEN)
