d9514428799a0c5fc0511e63b045eb224608e64f
albertoleoncio <albertoleoncio@hotmail.com>
2024-05-17 15:07:32 -0300

    Settings_local.py: +tests

--- CapX/settings_local.py
+++ CapX/settings_local.py
@@ -2,63 +2,81 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 HOME = os.environ.get('HOME') or ""
 SECRET_KEY = os.environ.get("SECRET_KEY")
 SOCIAL_AUTH_MEDIAWIKI_URL = 'https://meta.wikimedia.org/w/index.php'
 SOCIAL_AUTH_MEDIAWIKI_KEY = os.environ.get("SOCIAL_AUTH_MEDIAWIKI_KEY")
 SOCIAL_AUTH_MEDIAWIKI_SECRET = os.environ.get("SOCIAL_AUTH_MEDIAWIKI_SECRET")
-
-
-if os.path.exists(HOME + '/replica.my.cnf'):
-    DEBUG = False
-    ALLOWED_HOSTS = ['capx-backend.toolforge.org','toolforge.org']
-    SOCIAL_AUTH_MEDIAWIKI_CALLBACK = 'https://capx.toolforge.org/oauth/'
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ.get("TOOL_TOOLSDB_USER") + '__capx',
-            'USER': os.environ.get("TOOL_TOOLSDB_USER"),
-            'PASSWORD': os.environ.get("TOOL_TOOLSDB_PASSWORD"),
-            'HOST': 'tools.db.svc.wikimedia.cloud',
-            'PORT': '',
-        }
-    }
-
-    OPENSEARCH_DSL={
-        'default': {
-            'hosts': 'http://elasticsearch.svc.tools.eqiad1.wikimedia.cloud:80',
-            'http_auth': (
-                os.environ.get("TOOL_ELASTICSEARCH_USER", "default-value"), 
-                os.environ.get("TOOL_ELASTICSEARCH_PASSWORD", "default-value")
-            )
-        }
-    }
-
-else:
-    DEBUG = True
-    ALLOWED_HOSTS = ['127.0.0.1']
-    SOCIAL_AUTH_MEDIAWIKI_CALLBACK = 'http://127.0.0.1:8000/oauth/complete/mediawiki/'
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME':  Path(__file__).resolve().parent.parent / 'db.sqlite3',
-        }
-    }
-
-    OPENSEARCH_DSL={
-        'default': {
-            'hosts': 'http://localhost:9200'
-        }
-    }
-    print('replica.my.cnf file not found, running as local')
-
 LANGUAGES = (
     ('en', 'English'),
     ('pt-br', 'Brazilian Portuguese'),
     ('pt', 'Portuguese'),
     ('es', 'Spanish'),
 )
+
+def configure_settings():
+    if os.path.exists(HOME + '/replica.my.cnf'):
+        debug = False
+        hosts = ['capx-backend.toolforge.org','toolforge.org']
+        callback = 'https://capx.toolforge.org/oauth/'
+
+        databases = {
+            'default': {
+                'ENGINE': 'django.db.backends.mysql',
+                'NAME': os.environ.get("TOOL_TOOLSDB_USER") + '__capx', # type: ignore
+                'USER': os.environ.get("TOOL_TOOLSDB_USER"),
+                'PASSWORD': os.environ.get("TOOL_TOOLSDB_PASSWORD"),
+                'HOST': 'tools.db.svc.wikimedia.cloud',
+                'PORT': '',
+            }
+        }
+
+        opensearch={
+            'default': {
+                'hosts': 'http://elasticsearch.svc.tools.eqiad1.wikimedia.cloud:80',
+                'http_auth': (
+                    os.environ.get("TOOL_ELASTICSEARCH_USER", "default-value"), 
+                    os.environ.get("TOOL_ELASTICSEARCH_PASSWORD", "default-value")
+                )
+            }
+        }
+
+    else:
+        debug = True
+        hosts = ['127.0.0.1']
+        callback = 'http://127.0.0.1:8000/oauth/complete/mediawiki/'
+
+        databases = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME':  Path(__file__).resolve().parent.parent / 'db.sqlite3',
+            }
+        }
+
+        opensearch={
+            'default': {
+                'hosts': 'http://localhost:9200'
+            }
+        }
+
+    return {
+        'DEBUG': debug,
+        'ALLOWED_HOSTS': hosts,
+        'SOCIAL_AUTH_MEDIAWIKI_CALLBACK': callback,
+        'DATABASES': databases,
+        'OPENSEARCH_DSL': opensearch,
+    }
+
+settings = configure_settings()
+DEBUG = settings['DEBUG']
+ALLOWED_HOSTS = settings['ALLOWED_HOSTS']
+SOCIAL_AUTH_MEDIAWIKI_CALLBACK = settings['SOCIAL_AUTH_MEDIAWIKI_CALLBACK']
+DATABASES = settings['DATABASES']
+OPENSEARCH_DSL = settings['OPENSEARCH_DSL']
+if DEBUG == True:
+    print('replica.my.cnf file not found, running as local')
+else:
+    print('replica.my.cnf file found, running as production')
---
+++ CapX/tests/test_settings_local.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+from unittest.mock import patch
+import CapX.settings_local as settings_local
+
+class TestSettingsLocal(unittest.TestCase):
+    def test_os_path_exists_false(self):
+        with patch('os.path.exists', return_value=False):
+            settings = settings_local.configure_settings()
+            # Your assertions when os.path.exists returns False
+            self.assertFalse(os.path.exists('/some/path'))  # This will be False
+            self.assertListEqual(settings['ALLOWED_HOSTS'], ['127.0.0.1'])
+            self.assertEqual(settings['SOCIAL_AUTH_MEDIAWIKI_CALLBACK'], 'http://127.0.0.1:8000/oauth/complete/mediawiki/')
+            self.assertEqual(settings['DATABASES']['default']['ENGINE'], 'django.db.backends.sqlite3')
+            self.assertEqual(settings['OPENSEARCH_DSL']['default']['hosts'], 'http://localhost:9200')
+
+    def test_os_path_exists_true(self):
+        with patch('os.path.exists', return_value=True), patch('os.environ.get', return_value=""):
+            settings = settings_local.configure_settings()
+            # Your assertions when os.path.exists returns True
+            self.assertTrue(os.path.exists('/some/path'))  # This will be True
+            self.assertListEqual(settings['ALLOWED_HOSTS'], ['capx-backend.toolforge.org', 'toolforge.org'])
+            self.assertEqual(settings['SOCIAL_AUTH_MEDIAWIKI_CALLBACK'], 'https://capx.toolforge.org/oauth/')
+            self.assertEqual(settings['DATABASES']['default']['ENGINE'], 'django.db.backends.mysql')
+            self.assertEqual(settings['OPENSEARCH_DSL']['default']['hosts'], 'http://elasticsearch.svc.tools.eqiad1.wikimedia.cloud:80')
