9e9481f7baebd63fcec1d3cee49dd370a2f79adb
albertoleoncio <albertoleoncio@hotmail.com>
2024-05-16 22:11:49 -0300

    Metadata: +docs

--- CapX/metadata.py
+++ CapX/metadata.py
@@ -4,8 +4,26 @@ from django.utils.encoding import force_str
 
 
 class CustomMetadata(SimpleMetadata):
+    """
+    Custom metadata class that extends the SimpleMetadata class.
+
+    This class overrides the get_field_info method to provide choices to ManyRelatedField
+    instances temporarily on the OPTIONS method of the Django REST Framework (DRF).
+    This is done until a better method for providing choices is established.
+
+    """
 
     def get_field_info(self, field):
+        """
+        Get the metadata information for a given field.
+
+        Args:
+            field: The field for which to retrieve the metadata information.
+
+        Returns:
+            dict: The metadata information for the given field.
+
+        """
         field_info = super().get_field_info(field)
 
         if (not field_info.get('read_only') and
