32d52f5e83a49ec1009944af35f6e967f3056caa
albertoleoncio <albertoleoncio@hotmail.com>
2024-08-06 13:42:59 -0300

    refactor: Update UsersByTagViewSet to use object_or_404 for tag retrieval

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from .models import Profile, Territory, Language, WikimediaProject
 from .serializers import ProfileSerializer, TerritorySerializer, LanguageSerializer, WikimediaProjectSerializer, UsersBySkillSerializer
 from rest_framework import status, viewsets, filters
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 
 class UsersViewSet(viewsets.ModelViewSet):
@@ -105,9 +106,11 @@ class UsersBySkillViewSet(viewsets.ReadOnlyModelViewSet):
 
     def retrieve(self, request, *args, **kwargs):
         skill_id = self.kwargs['pk']
-        known_users = Profile.objects.filter(skills_known=skill_id)
-        available_users = Profile.objects.filter(skills_available=skill_id)
-        wanted_users = Profile.objects.filter(skills_wanted=skill_id)
+        skill = get_object_or_404(Skill, pk=skill_id)
+
+        known_users = Profile.objects.filter(skills_known=skill)
+        available_users = Profile.objects.filter(skills_available=skill)
+        wanted_users = Profile.objects.filter(skills_wanted=skill)
         data = {
             'known': [{'id': user.id, 'display_name': user.display_name, 'username': user.user.username, 'profile_image': user.profile_image} for user in known_users],
             'available': [{'id': user.id, 'display_name': user.display_name, 'username': user.user.username, 'profile_image': user.profile_image} for user in available_users],
@@ -135,25 +138,30 @@ class UsersByTagViewSet(viewsets.ReadOnlyModelViewSet):
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
         if tag_type == 'skill':
-            known_users = Profile.objects.filter(skills_known=tag_id)
-            available_users = Profile.objects.filter(skills_available=tag_id)
-            wanted_users = Profile.objects.filter(skills_wanted=tag_id)
+            tag = get_object_or_404(Skill, pk=tag_id)
+            known_users = Profile.objects.filter(skills_known=tag)
+            available_users = Profile.objects.filter(skills_available=tag)
+            wanted_users = Profile.objects.filter(skills_wanted=tag)
             data = {
                 'known': [{'id': user.id, 'display_name': user.display_name, 'username': user.user.username, 'profile_image': user.profile_image} for user in known_users],
                 'available': [{'id': user.id, 'display_name': user.display_name, 'username': user.user.username, 'profile_image': user.profile_image} for user in available_users],
                 'wanted': [{'id': user.id, 'display_name': user.display_name, 'username': user.user.username, 'profile_image': user.profile_image} for user in wanted_users],
             }
         elif tag_type == 'language':
-            users = Profile.objects.filter(language=tag_id)
+            tag = get_object_or_404(Language, pk=tag_id)
+            users = Profile.objects.filter(language=tag)
             data = [{'id': user.id, 'display_name': user.display_name, 'username': user.user.username, 'profile_image': user.profile_image} for user in users]
         elif tag_type == 'territory':
-            users = Profile.objects.filter(territory=tag_id)
+            tag = get_object_or_404(Territory, pk=tag_id)
+            users = Profile.objects.filter(territory=tag)
             data = [{'id': user.id, 'display_name': user.display_name, 'username': user.user.username, 'profile_image': user.profile_image} for user in users]
         elif tag_type == 'wikimedia_project':
-            users = Profile.objects.filter(wikimedia_project=tag_id)
+            tag = get_object_or_404(WikimediaProject, pk=tag_id)
+            users = Profile.objects.filter(wikimedia_project=tag)
             data = [{'id': user.id, 'display_name': user.display_name, 'username': user.user.username, 'profile_image': user.profile_image} for user in users]
         elif tag_type == 'affiliation':
-            users = Profile.objects.filter(affiliation=tag_id)
+            tag = get_object_or_404(Organization, pk=tag_id)
+            users = Profile.objects.filter(affiliation=tag)
             data = [{'id': user.id, 'display_name': user.display_name, 'username': user.user.username, 'profile_image': user.profile_image} for user in users]
         else:
             response = {'message': 'Invalid tag type.'}
