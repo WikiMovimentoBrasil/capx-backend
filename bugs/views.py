129df1cef0efd4a49c8ddfd32fa3964683b3a9da
albertoleoncio <albertoleoncio@hotmail.com>
2024-09-21 15:54:15 -0300

    refactor: Remove queryset assignment in BugViewSet

--- bugs/views.py
+++ bugs/views.py
@@ -6,7 +6,6 @@ from rest_framework.response import Response
 
 class BugViewSet(viewsets.ModelViewSet):
     serializer_class = BugSerializer
-    queryset = Bug.objects.all()
 
     def get_queryset(self):
         user = self.request.user
@@ -19,20 +18,27 @@ class BugViewSet(viewsets.ModelViewSet):
     # Check if user is staff
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
-
-        if not request.user.is_staff:
-            return Response(
-                {"detail": "You do not have permission to perform this action."},
-                status=status.HTTP_403_FORBIDDEN
-            )
-        
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-        
+        if request.user.is_staff:
+            return super().destroy(request, *args, **kwargs)
+        return Response(
+            {"detail": "You do not have permission to perform this action."},
+            status=status.HTTP_403_FORBIDDEN
+        )       
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user.is_staff:
+            return super().update(request, *args, **kwargs)
+        return Response(
+            {"detail": "You do not have permission to perform this action."},
+            status=status.HTTP_403_FORBIDDEN
+        )
+
+    def partial_update(self, request, *args, **kwargs):
+        return response.Response("PATCH method not allowed", status=status.HTTP_405_METHOD_NOT_ALLOWED) 
 
 class AttachmentViewSet(viewsets.ModelViewSet):
     serializer_class = AttachmentSerializer
@@ -44,3 +50,18 @@ class AttachmentViewSet(viewsets.ModelViewSet):
         else:
             queryset = Attachment.objects.filter(bug__user=user)
         return queryset
+
+    def partial_update(self, request, *args, **kwargs):
+        return response.Response("PATCH method not allowed", status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def update(self, request, *args, **kwargs):
+        return response.Response("PUT method not allowed", status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user.is_staff:
+            return super().destroy(request, *args, **kwargs)
+        return Response(
+            {"detail": "You do not have permission to perform this action."},
+            status=status.HTTP_403_FORBIDDEN
+        )
