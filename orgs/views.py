de426fae6097baef33504a23813a7b41f0da55bc
albertoleoncio <albertoleoncio@hotmail.com>
2024-09-21 14:19:25 -0300

    refactor: Simplify OrganizationViewSet and ListOrganizationViewSet

--- orgs/views.py
+++ orgs/views.py
@@ -15,7 +15,7 @@ class OrganizationViewSet(viewsets.ModelViewSet):
             return Organization.objects.all()
         else:
             return Organization.objects.filter(managers__isnull=False)
-        
+    
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
@@ -24,22 +24,27 @@ class OrganizationViewSet(viewsets.ModelViewSet):
         data['managers'] = [User.objects.get(id=id).username for id in data['managers']]
         return Response(data)
 
-    def get_permissions(self):
-        if self.request.method in ['DELETE', 'POST']:
-            return [permissions.IsAdminUser()]
-        return [permissions.IsAuthenticated()]
+    def create(self, request, *args, **kwargs):
+        if request.user.is_staff:
+            return super().create(request, *args, **kwargs)
+        return Response("You do not have permission to create an organization.", status=status.HTTP_403_FORBIDDEN)
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
         if request.user.is_staff or request.user in instance.managers.all():
             return super().update(request, *args, **kwargs)
+        return Response("You do not have permission to update this organization.", status=status.HTTP_403_FORBIDDEN)
         
     def partial_update(self, request, *args, **kwargs):
+        return response.Response("PATCH method not allowed", status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
-        if request.user.is_staff or request.user in instance.managers.all():
-            return super().partial_update(request, *args, **kwargs)
-        
-        
+        if request.user.is_staff:
+            return super().destroy(request, *args, **kwargs)
+        return Response("You do not have permission to delete this organization.", status=status.HTTP_403_FORBIDDEN)
+
+
 class ListOrganizationViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Organization.objects.all()
     serializer_class = OrganizationSerializer
@@ -48,6 +53,9 @@ class ListOrganizationViewSet(viewsets.ReadOnlyModelViewSet):
         queryset = self.get_queryset()
         data = {org.id: str(org) for org in queryset}
         return Response(data)
+
+    def retrieve(self, request, *args, **kwargs):
+        return Response("Method not allowed", status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 class OrganizationTypeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = OrganizationType.objects.all()
